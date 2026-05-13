8b92d6caccb78c644afe6e9ea9b7ddfba3bf4387
Lokesh Sai Srinivas <ayikalokeshsaisrinivas542@gmail.com>
2026-05-13 12:51:40 +0530

    Remove hardcoded Firebase secrets and update .gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTQRmLC5O_xLSfOj4Nebj0ZuNU7RJK1r0",
-  authDomain: "nano-heal-5440b.firebaseapp.com",
-  projectId: "nano-heal-5440b",
-  storageBucket: "nano-heal-5440b.firebasestorage.app",
-  messagingSenderId: "343883438881",
-  appId: "1:343883438881:web:84e97d8e0da95fa3ccfc60",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
